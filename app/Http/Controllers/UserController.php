cff6bb54ee27634e3c0166136ae2af3f9d654615
vagrant <vagrant@weibo>
2019-09-22 10:49:45 +0000

    2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +10,8 @@ class UserController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user){
+        return view('users.show',compact('user'));
     }
 }
