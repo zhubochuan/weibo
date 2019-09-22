e08edc16ad779a1b179bf93207faf9ce7d24000d
zhubochuan <2536167503@qq.com>
2019-09-22 13:26:00 +1000

    update

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="float-right">
+        <a href="/about">about</a>
+    </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,17 +7,12 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Weibo App</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
-            </ul>
-        </div>
-    </nav>
+    @include('layouts._header')
     <div class="container">
-        @yield('content')
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
 </body>
 
