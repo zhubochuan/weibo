47aa12aa738e14d47fecffd1ecba6a89290e6e9b
vagrant <vagrant@weibo>
2019-09-22 10:53:55 +0000

    3

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.default')
-@section('title', 'signup')
+@section('title', 'sign up')
 
 @section('content')
-<h1>signup</h1>
+<div class="offset-md-2 col-md-8">
+    <div class="card ">
+        <div class="card-header">
+            <h5>sign up</h5>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('users.store') }}">
+                <div class="form-group">
+                    <label for="name">name：</label>
+                    <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">e-mail/gmail：</label>
+                    <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">password：</label>
+                    <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">confirm password：</label>
+                    <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                </div>
+
+                <button type="submit" class="btn btn-primary">sign up</button>
+            </form>
+        </div>
+    </div>
+</div>
 @stop
